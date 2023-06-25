5bb24c1d1cc0a33ceb0cc6d6b4a1d003922809f0
Cindypermata <113223444+Cindypermata@users.noreply.github.com>
2023-06-25 11:21:44 +0700

    Add files via upload

---
+++ BeliAyam.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package beliayam;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Admin
+ */
+public class BeliAyam {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+            System.out.println("selamat datang di geprek kamal");
+            System.out.println("1. Pesan Menu");
+            System.out.println("2. Lihat Pesanan");
+            System.out.println("3. Selesaikan pesanan");
+            System.out.println("Selesaikan Pemesanan");
+            System.out.println("pilih menu (1-3): 1");
+            Pesanan.ayam();
+            System.out.println("Apakah tambah nasi (ya/tidak? tidak");
+            
+            
+            
+ 
+    }
+    
+}
---
+++ Pesanan.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package beliayam;
+
+/**
+ *
+ * @author Admin
+ */
+public class Pesanan{
+    
+    public static void ayam(){
+        System.out.println("1. Biasa Rp.5000");
+        System.out.println("2. Double Rp.7000");
+        System.out.println("3. Special Rp.9000");
+    }
+    
+}
